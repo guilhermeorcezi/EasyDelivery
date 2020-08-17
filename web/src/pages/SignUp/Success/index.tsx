39669a4d27ed1d30c6e1a35d3b5bb493d383156e
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-08-17 11:09:56 -0300

    Page sign-up-success created

---
+++ web/src/pages/SignUp/Success/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+import imgSignUpSucess from '../../../assets/images/signup-success.png'
+
+import './styles.css';
+
+const Success: React.FC = () => {
+	return (
+		<div id="page-signup-success">
+			<div className="container">
+				<div className="content">
+					<img src={imgSignUpSucess} alt="Sign Up Success"/>
+					<h1>Oops! Página não encontrada...</h1>
+					<p>
+						Mas fique tranquilo! Por que não tenta retornar para a página
+						inicial para encontrar outro caminho?
+					</p>
+				</div>
+				<div className="footer">
+					<Link to="/">Fazer Login</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Success;
--- web/src/routes.js
+++ web/src/routes.js
@@ -3,12 +3,14 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Page404 from './pages/Page404';
+import Success from './pages/SignUp/Success';
 
 function Routes() {
 	return (
 		<BrowserRouter>
 			<Switch>
 				<Route path="/" exact component={Login} />
+				<Route path="/success" exact component={Success} />
 				<Route component={Page404} />
 			</Switch>
 		</BrowserRouter>
