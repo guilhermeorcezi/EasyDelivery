13df87b765d4ef303c343c06802110393c4b07d6
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-08-17 10:48:40 -0300

    Page Error404 created

--- web/src/pages/Page404/index.tsx
+++ web/src/pages/Page404/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+import errorAnimation from '../../assets/lotties/error404.json';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
 
 const Page404: React.FC = () => {
-  return <div />;
-}
+	const defaulOptions = {
+		loop: true,
+		autoplay: true,
+		animationData: errorAnimation,
+		rendererSettings: {
+			preserveAspectRatio: 'xMidYMid slice',
+		},
+	};
+
+	return (
+		<div id="page-404-error">
+			<div className="container">
+				<div className="content">
+					<Lottie options={defaulOptions} height={120} width={120} />
+					<h1>Oops! Página não encontrada...</h1>
+					<p>
+						Mas fique tranquilo! Por que não tenta retornar para a página
+						inicial para encontrar outro caminho?
+					</p>
+				</div>
+				<div className="footer">
+					<Link to="/">Página Inicial</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Page404;
--- web/src/routes.js
+++ web/src/routes.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import {Route, BrowserRouter} from 'react-router-dom'
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
-import Login from './pages/Login'
-import Page404 from './pages/Page404'
+import Login from './pages/Login';
+import Page404 from './pages/Page404';
 
 function Routes() {
-  return (
-      <BrowserRouter>
-      <Route path="/" component={Login}/>
-      <Route component={Page404}/>
-      </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" exact component={Login} />
+				<Route component={Page404} />
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default Routes;
