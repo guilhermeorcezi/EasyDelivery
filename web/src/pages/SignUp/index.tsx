2d512f7edef7dc88a6e5a947495b62b2c55e08ee
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-08-18 15:05:02 -0300

    dropzone file upload component added and client-user sign-up page created

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,13 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import InputStyled from '../../components/Input'
+import goBackImg from '../../assets/images/goback.png';
 
-import'./styles.css';
+import Input from '../../components/Input';
+import PasswordInput from '../../components/PasswordInput';
+import Dropzone from '../../components/Dropzone';
+
+import './styles.css';
 
 const SignUp: React.FC = () => {
-  return (
-   <h1>oi</h1>
-  )
-}
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [whatsapp, setWhatsapp] = useState('');
+	const [selectedFile, setSelectedFile] = useState<File>();
+
+	function handleSignUp() {}
+
+	return (
+		<main id="page-user-sign-up">
+			<div className="form-side">
+				<img src={goBackImg} />
+
+				<form onSubmit={handleSignUp}>
+					<h1>Cadastre-se</h1>
+
+					<div className="avatar-block">
+						<Dropzone onFileUploaded={setSelectedFile} />
+					</div>
+
+					<div className="input-group">
+						<div className="input-block">
+							<Input
+								type="text"
+								placeholder="Nome"
+								name="name"
+								label="Nome"
+								value={name}
+								onChange={(e) => setName(e.target.value)}
+							/>
+						</div>
+						<div className="input-block">
+							<Input
+								type="text"
+								placeholder="E-mail"
+								name="email"
+								label="E-mail"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+						</div>
+					</div>
+
+					<div className="input-group">
+						<div className="input-icon">
+							<PasswordInput />
+						</div>
+
+						<div className="input-block">
+							<Input
+								type="text"
+								placeholder="Whatsapp"
+								name="whatsapp"
+								label="Whatsapp"
+								value={whatsapp}
+								onChange={(e) => setWhatsapp(e.target.value)}
+							/>
+						</div>
+					</div>
+
+					<button type="submit">Entrar</button>
+				</form>
+
+				<div className="footer">
+					<div className="register-block">
+						<span>Já possui conta?</span>
+						<a href="/">Efetuar Login</a>
+					</div>
+				</div>
+			</div>
+
+			<div className="banner-side">EasyDelivery</div>
+		</main>
+	);
+};
 
 export default SignUp;
