24cffe51b0dbc3a9583386c85b76c2bc60bd6131
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-08-19 21:15:23 -0300

    page profile version 1 created

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -4,32 +4,51 @@ import imgAvatar from '../../assets/images/user-avatar.png';
 import logoutIcon from '../../assets/icons/logout-icon.png';
 import goBackIcon from '../../assets/icons/goback-light.png';
 import imgLogo from '../../assets/images/logo.png';
+import imgAvatarUpload from '../../assets/icons/avatar-upload.png';
 
 import './styles.css';
 interface Props {
 	user?: string;
+	profile?: boolean;
 }
 
-const Header: React.FC<Props> = ({ user, children }) => {
+const Header: React.FC<Props> = ({ user, children, profile }) => {
 	return (
-		<header id="header" className={children ? 'header' : ''}>
-			<div className="left-side">
-				<img
-					src={children ? goBackIcon : imgAvatar}
-					className={children ? 'goBack' : ''}
-					alt={children ? 'Go Back' : 'User'}
-				/>
-				{user && <span>{user}</span>}
-			</div>
-			<div className="children">{children}</div>
-			<div className="right-side">
-				<img
-					src={children ? imgLogo : logoutIcon}
-					alt={children ? 'Easy Delivery' : 'LogOut'}
-					className={children ? 'logo' : ''}
-				/>
-			</div>
-		</header>
+		<>
+			<header id="header" className={children ? 'header' : ''}>
+				<div className="left-side">
+					<img
+						src={children ? goBackIcon : imgAvatar}
+						className={children ? 'goBack' : ''}
+						alt={children ? 'Go Back' : 'User'}
+					/>
+					{user && <span>{user}</span>}
+				</div>
+				<div className="children">{children}</div>
+				<div className="right-side">
+					<img
+						src={children ? imgLogo : logoutIcon}
+						alt={children ? 'Easy Delivery' : 'LogOut'}
+						className={children ? 'logo' : ''}
+					/>
+				</div>
+			</header>
+			{children && profile && (
+				<div id="header-content">
+					<div className="profile-content">
+						<img src={imgAvatar} className="profile-avatar" alt="User" />
+						<div className="upload-block">
+							<img
+								src={imgAvatarUpload}
+								className="avatar-upload"
+								alt="Upload"
+							/>
+						</div>
+						<span>Guilherme Orcezi</span>
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
--- web/src/pages/Profile/index.tsx
+++ web/src/pages/Profile/index.tsx
@@ -1,9 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
+import Header from '../../components/Header';
+import Input from '../../components/Input';
 
 const Profile: React.FC = () => {
-	return <h1>Profile</h1>
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [whatsapp, setWhatsapp] = useState('');
+	const [uf, setUf] = useState('');
+	const [city, setCity] = useState('');
+
+	function handleEditProfile() {}
+
+	return (
+		<div id="page-profile">
+			<Header profile>Meu Perfil</Header>
+			<div className="container">
+				<form onSubmit={handleEditProfile}>
+					<span>Seus dados</span>
+
+					<div className="input-block">
+						<legend>Nome</legend>
+						<Input
+							type="text"
+							placeholder="Nome"
+							name="name"
+							label="Nome"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+					</div>
+
+					<div className="input-block">
+						<legend>Email</legend>
+						<Input
+							type="text"
+							placeholder="Email"
+							name="email"
+							label="Email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</div>
+
+					<div className="input-block">
+						<legend>Nova senha</legend>
+						<Input
+							type="text"
+							placeholder="Nova Senha"
+							name="password"
+							label="Nova senha"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</div>
+
+					<div className="input-block">
+						<legend>Whatsapp</legend>
+						<Input
+							type="text"
+							placeholder="Whatsapp"
+							name="whatsapp"
+							label="Whatsapp"
+							value={whatsapp}
+							onChange={(e) => setWhatsapp(e.target.value)}
+						/>
+					</div>
+
+					<div className="input-block">
+						<legend>Seu estado</legend>
+						<Input
+							type="text"
+							placeholder="Uf"
+							name="uf"
+							label="Uf"
+							value={uf}
+							onChange={(e) => setUf(e.target.value)}
+						/>
+					</div>
+
+					<div className="input-block">
+						<legend>Sua cidade</legend>
+						<Input
+							type="text"
+							placeholder="City"
+							name="city"
+							label="City"
+							value={city}
+							onChange={(e) => setCity(e.target.value)}
+						/>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
