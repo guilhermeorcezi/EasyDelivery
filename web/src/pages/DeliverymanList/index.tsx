2db6be569145b20915ed0e3584397e0e4354bb29
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-08-20 14:47:03 -0300

    header component style updated: margin-bottom

--- web/src/pages/DeliverymanList/index.tsx
+++ web/src/pages/DeliverymanList/index.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import './styles.css';
+import UfInput from '../../components/UfInput';
+import CityInput from '../../components/CityInput';
 import Header from '../../components/Header';
 
+import './styles.css';
+
 const DeliverymanList: React.FC = () => {
+	const [uf, setUf] = useState('');
+	const [city, setCity] = useState('');
+
 	return (
-        <div id="page-list">
-            <Header list deliveryman_amount={2}>Buscar entregador</Header>
-        </div>
-    )
+		<div id="page-list">
+			<Header list deliveryman_amount={2}>
+				Buscar entregador
+			</Header>
+			<div className="container">
+				
+                <div className="search-group">
+					<div className="input-group">
+						<div className="input-block">
+							<legend>Seu estado</legend>
+							<UfInput selectedUf={uf} setSelectedUf={setUf} />
+						</div>
+						<div className="input-block">
+							<legend>Sua cidade</legend>
+							<CityInput
+								selectedUF={uf}
+								selectedCity={city}
+								setSelectedCity={setCity}
+							/>
+						</div>
+					</div>
+				</div>
+
+				<div className="list-wrapper"></div>
+			</div>
+		</div>
+	);
 };
 
 export default DeliverymanList;
