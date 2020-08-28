fd6e208a03934e4c4caa0e161936de88940ebb8a
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-08-28 16:28:22 -0300

    user auth working

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = express.Router();
 
 routes.post('/user', usersRouter);
+routes.post('/sessions', sessionsRouter);
 
 export default routes;
